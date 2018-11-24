fae99e3d52bc9c420d8239aab2bda3c89f223751
Rilquer Mascarenhas <rilquermascarenhas@hotmail.com>
2018-11-24 14:42:33 -0300

    Update Eliminando localidades próximas.R

---
+++ Eliminando localidades próximas.R
@@ -0,0 +1,7 @@
+#Workflow do Script
+##1. Calcular matriz de dist?ncia geogr?fica entre os pontos, ou importar essa matriz
+##de outro programa.
+##2. Calcular para cada localidade quantas localidades est?o a menos de x km de dist?ncia,
+##3. Ordenar as localidades por aquelas que est?o mais pr?ximas de outras (ordenar quant
+##original.
+##uma localidade de cada par, de modo aleat?rio.
